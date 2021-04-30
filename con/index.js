33cbfe1c4626fdbb129273b8c780e8fceb37a7fb
karuiner <intaekgong@gmail.com>
2021-04-30 15:28:05 +0900

    remove

--- con/index.js
+++ con/index.js
@@ -12,19 +12,6 @@ const Seq = require("sequelize"); //{Op , ...}
 router.use("/user", user);
 router.use("/search", search);
 
-// crypto.randomBytes(64, (err, buf) => {
-//   crypto.pbkdf2(
-//     "비밀번호",
-//     buf.toString("base64"),
-//     100000,
-//     64,
-//     "sha512",
-//     (err, key) => {
-//       console.log(key.toString("base64")); // 'dWhPkH6c4X1Y71A/DrAHhML3DyKQdEkUOIaSmYCI7xZkD5bLZhPF0dOSs2YZA/Y4B8XNfWd3DHIqR5234RtHzw=='
-//     }
-//   );
-// });
-
 router.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
