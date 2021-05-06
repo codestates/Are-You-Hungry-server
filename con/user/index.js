34c08b69577688c233206abbd99a346c225a6511
karuiner <intaekgong@gmail.com>
2021-05-06 16:26:20 +0900

    비번 수정 픽스

--- con/user/index.js
+++ con/user/index.js
@@ -39,22 +39,43 @@ router.get("/", (req, res) => {
 router.patch("/", (req, res) => {
   let { id, username } = res.locals;
 
-  Models.User.update(
-    {
-      ...req.body,
-    },
-    {
-      where: {
-        [Op.and]: [{ id: id }, { username: username }],
-      },
-    }
-  )
-    .then((rst) => {
-      res.status(200).json({ message: "information updated" });
-    })
-    .catch((err) => {
-      res.status(400).send("fail");
+  try {
+    crypto.randomBytes(64, (err, buf) => {
+      crypto.pbkdf2(
+        req.body.password,
+        buf.toString("base64"),
+        120900,
+        64,
+        "sha512",
+        (err, key) => {
+          Models.User.findOne({ where: { username: req.body.username } }).then(
+            (rst) => {
+              Models.User.update(
+                {
+                  ...req.body,
+                  password: key.toString("base64"),
+                  password2: buf.toString("base64"),
+                },
+                {
+                  where: {
+                    [Op.and]: [{ id: id }, { username: username }],
+                  },
+                }
+              )
+                .then((rst) => {
+                  res.status(200).json({ message: "information updated" });
+                })
+                .catch((err) => {
+                  res.status(400).send("fail");
+                });
+            }
+          );
+        }
+      );
     });
+  } catch {
+    res.status(400).send("fail");
+  }
 });
 
 router.delete("/", (req, res) => {
